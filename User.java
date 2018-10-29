7ee56abe9dd7d55d79ffb5ba75fd83eee39b2eb3
RachelRogers220 <40871447+RachelRogers220@users.noreply.github.com>
2018-10-28 20:45:46 -0500

    Add files via upload
    MicroBlog assignment.

---
+++ User.java
@@ -0,0 +1,54 @@
+import java.util.ArrayList;
+
+public class User
+{
+    private String username;
+    private String fullName;
+    private String email;
+    private String avatarURl;
+    //private Post[] posts = new Post[100];
+    int counter = 0;
+    private ArrayList posts = new ArrayList();
+
+    public User(String username, String fullName, String email, String avatarURl)
+    {
+        this.username = username;
+        this.fullName = fullName;
+        this.email = email;
+        this.avatarURl = avatarURl;
+    }
+
+    //void addPost(Post p)
+    public void addPost(Post p)
+    {
+        posts.add(p);
+
+    }
+
+    public ArrayList getPosts()
+    {
+        return posts;
+    }
+
+    public String getUsername()
+    {
+        return username;
+    }
+
+    public String getFullName()
+    {
+        return fullName;
+    }
+
+    public String getEmail()
+    {
+
+        return email;
+    }
+
+    public String getAvatarURl()
+    {
+
+        return avatarURl;
+    }
+}
